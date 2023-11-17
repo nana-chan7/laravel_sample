fc0b9c8d87528f67699c70b43f698b7d4d2a896a
frogBear_7 <{220098tt@yse-c.net}>
2023-11-17 13:44:31 +0900

    up

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
@@ -11,8 +12,13 @@ class ItemController extends Controller
      */
     public function index()
     {
+        // TODO: データをすべて取得
+        // SELECT * FROM items;
+        $items = Item::get();
+        $data['items'] = $items;
+
         // views/item/index.blade.php
-        return view('item.index');
+        return view('item.index', $data);
     }
 
     /**
@@ -29,10 +35,15 @@ class ItemController extends Controller
      */
     public function store(Request $request)
     {
-        //TODO: Requestからデータを取得
-        //TODO: データベースに保存
-        //TODO: リダイレクト
-        dd($request);
+        // dd($request);
+        // dd($request->all());
+        //Requestからデータを取得
+        $data = $request->all();
+        //データベースに保存
+        // INSERT INTO items (name, price) VALUES (xxxx, xxxx);
+        Item::create($data);
+        //リダイレクト
+        return redirect(route('item.index'));
     }
 
     /**
