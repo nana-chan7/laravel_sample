201d215c86a0da667db40d0a6755bd2de695dbbd
frogBear_7 <{220098tt@yse-c.net}>
2023-11-17 12:18:28 +0900

    up

--- database/migrations/2023_11_17_030327_create_items_table.php
+++ database/migrations/2023_11_17_030327_create_items_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name', 255);
+            $table->integer('price');
         });
     }
 
