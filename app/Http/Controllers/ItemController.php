147f550b42e3f78702e4cff0e6674cd3ba2e52dc
frogBear_7 <{220098tt@yse-c.net}>
2023-11-17 11:28:25 +0900

    up

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -1,9 +1,7 @@
 <?php
-// 2023-10-13 3.week4
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
@@ -35,24 +33,24 @@ class ItemController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(int $id)
     {
-        // $item[1] = 'コーヒー';
-        // $item[2] = '紅茶';
-        // $item[3] = 'ほうじ茶';
+        // $items[1] = "コーヒー";
+        // $items[2] = "紅茶";
+        // $items[3] = "ほうじ茶";
         $items = [
             1 => 'コーヒー',
             2 => '紅茶',
             3 => 'ほうじ茶',
         ];
         $item = "";
-        if ($id > 0 && in_array($id, array_keys($items))) { //リストにない値が来た場合、空白で返す
+        if ($id > 0 && in_array($id, array_keys($items))) {
             $item = $items[$id];
         }
-        // ビューに受け渡すデータを生成
+        // ビューに受け渡すデータ生成
         $data = ['item' => $item];
 
-        // resouces/view/item/show.blade.php を表示
+        // reources/views/item/show.blade.php を表示
         // データを受け渡す
         return view('item.show', $data);
     }
